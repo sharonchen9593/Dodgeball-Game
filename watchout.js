78a2c6e0b034dd7d85e378b4bb434bdbb1e57477
sharonchen9593 <sharonchen9593@gmail.com>
2017-05-30 20:11:59 -0700

    Created enemies(balls) that move around

---
+++ watchout.js
@@ -0,0 +1,67 @@
+function runGame() {
+
+
+  var nEnemies = 20;
+  var highScore = 0;
+  var collisions = 0;
+  var currentScore = 0;
+
+
+
+  // draw enemies in as SVG element
+
+  var h = window.innerHeight-100   // height in px of browser window
+  var w = window.innerWidth-100    // width of browser window
+
+
+  // get coordinates to randomly put svg element
+  var randomX = function() {
+    return Math.random() * w;
+  }
+
+  var randomY = function() {
+    return Math.random() * h;
+  }
+
+  // build game window
+  var gameWindow = d3
+    .select('body')
+    .append('svg')
+    .attr({
+      width: w,
+      height: h,
+    })
+
+  // put enemies in
+
+  var enemies = gameWindow
+    .selectAll('circle')
+    .data(d3.range(nEnemies))
+    .enter()
+    .append('circle')
+    .style({
+      cx: randomX,
+      cy: randomY,
+      r: 15
+    })
+
+
+  var move = function() {
+    enemies
+    .transition().duration(2000)
+    .style({
+      cx: randomX,
+      cy: randomY,
+      r: 15
+    })
+    .transition()
+    .duration(100)
+    .each('end', move)
+  }
+  move()
+
+}
+
+runGame()
+
+
